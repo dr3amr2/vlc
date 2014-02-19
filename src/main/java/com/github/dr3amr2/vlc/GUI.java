b8984c5f78e2a9e82f3190bec20bd606489ff8cf
dr3amr2 <dnguyen4@du.edu>
2014-02-19 16:42:25 -0700

    GUI implementation

---
+++ src/main/java/com/github/dr3amr2/vlc/GUI.java
@@ -0,0 +1,31 @@
+package com.github.dr3amr2.vlc;
+
+/**
+ * Created by Dizzy on 2/19/14.
+ *
+ */
+import com.sun.jna.NativeLibrary;
+
+import java.util.Properties;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class GUI {
+    public static void main(String[] args) {
+        NativeLibrary.addSearchPath("libvlc", "C:\\Program Files\\VideoLAN\\VLC");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Properties props = System.getProperties();
+                props.setProperty("vlcj.log", "DEBUG");
+                JFrame frame = new JFrame();
+                Player p = new Player(frame);
+                p.createToolTip(); // actually doing nothing
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
